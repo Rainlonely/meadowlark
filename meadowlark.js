1a660832abebde17033fff434af9bb7c446f7e2f
Rain Wu <rainlonely@me.com>
2015-10-16 17:35:03 +0800

    add little

--- meadowlark.js
+++ meadowlark.js
@@ -5,8 +5,19 @@ var handlebars = require('express3-handlebars')
 				.create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.use(express.static(__dirname+'/public'));
+app.use(require('body-parser')());
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
+
+
+
+app.post('/process', function(req, res){
+	if(req.xhr || req.accepts('json,html')==='json'){
+		res.send({ success: true }); 
+	} else {
+		res.redirect(303, '/thank-you');
+	}
+});
 
 app.use(function(req, res, next){
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test == '1';
@@ -35,6 +46,18 @@ app.get('/about', function(req, res){
 		fortune : fortune.getFortune(),
 		pageTestScript: '/qa/tests-about.js' 
 	});
+});
+
+app.get('/newsletter', function(req, res){
+	res.render('newsletter', {csrf: 'CSRF token goes there'});
+});
+
+app.post('/process', function(req, res){
+	console.log('Form (from querystring): ' + req.query.form); 
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf); 
+	console.log('Name (from visible form field): ' + req.body.name); 
+	console.log('Email (from visible form field): ' + req.body.email);
+	res.redirect(303, '/thank-you');
 });
 
 app.get('/tours/hood-river', function(req, res){
